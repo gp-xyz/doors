d8dabcb7b53b8ee540ec30645f92ef7fdbd468d6
gp-xyz <102692598+gp-xyz@users.noreply.github.com>
2022-03-30 11:46:12 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 var palet1 = ["#d65780","#c9b1bd","#9dd1f1","#f4b860","#aceb98"];
 var greengrey = ["#6b6054","#929487","#a1b0ab","#c3dac3","#d5ecd4"];
-var browns = ["#452103","#210f04","#101010"]
+var browns = ["#452103","#210f04","#652103","#320f04","#101010"]
 var doorWidth = 360;
 var doorHeight = 800;
 var doorPane = .88
@@ -11,9 +11,9 @@ function setup() {
   createCanvas(1000, 1000);
   background(255);
   var colorchoice = random([palet1,greengrey,browns])
-  var myVsplit = random([0,1,2,11]);
-  var myHsplit = random([0,1,2,11]);
-  var aaa = new DoorWay(browns,"style",1,1);
+  var myVsplit = random([0,0,1,1,2,11,30]);
+  var myHsplit = random([0,0,1,1,2,11,30]);
+  var aaa = new DoorWay(colorchoice,"style",myVsplit,myHsplit);
   aaa.show();
   // noStroke();
   // slightlyBevelRectangle(0,0,360,800,dc1,dc2);
